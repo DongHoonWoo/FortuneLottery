a6bf897609f2d42abcd6ec82b75187376f9a86f6
Woo.DongHoon <dhwoo@nhn.com>
2012-12-13 22:36:07 +0900

    add lotto.csv

--- src/com/happyty/fortunelottery/MainActivity.java
+++ src/com/happyty/fortunelottery/MainActivity.java
@@ -1,9 +1,22 @@
 package com.happyty.fortunelottery;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -21,7 +34,69 @@ public class MainActivity extends Activity {
 		addBook(this);
 		showBooks(this);
 
+		Button load = (Button)findViewById(R.id.load_csv);
+		load.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				loadLottoCSV();
+			}
+		});
+
 		return true;
+	}
+
+	private void loadLottoCSV() {
+		AssetManager am = getAssets();
+		try {
+			InputStream is = am.open("lotto.csv");
+			Reader reader = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(reader);
+
+			String line;
+			while ((line = br.readLine()) != null) {
+				//				Log.d("Lotto", line);
+				parseLine(line);
+			}
+		} catch (IOException e) {
+			Log.e("Lotto", e.toString());
+		}
+	}
+
+	private void parseLine(String line) {
+		String[] sep = line.replace(" ", "").split(",");
+
+		int num;
+		Date date = new Date();
+		int n1, n2, n3, n4, n5, n6, bonus;
+
+		SimpleDateFormat format = new SimpleDateFormat("yyyy.MM.dd");
+
+		num = Integer.parseInt(sep[0]);
+		try {
+			date = format.parse(sep[1]);
+			date.setHours(20);
+			date.setMinutes(40);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		n1 = Integer.parseInt(sep[2]);
+		n2 = Integer.parseInt(sep[3]);
+		n3 = Integer.parseInt(sep[4]);
+		n4 = Integer.parseInt(sep[5]);
+		n5 = Integer.parseInt(sep[6]);
+		n6 = Integer.parseInt(sep[7]);
+		bonus = Integer.parseInt(sep[8]);
+
+		Log.d("Lotto", "parse =>" + num
+			+ "|" + date.toString()
+			+ "|" + n1
+			+ "|" + n2
+			+ "|" + n3
+			+ "|" + n4
+			+ "|" + n5
+			+ "|" + n6
+			+ "|" + bonus);
 	}
 
 	public void addBook(Context context)
