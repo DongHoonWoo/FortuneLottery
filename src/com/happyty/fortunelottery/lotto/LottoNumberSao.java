b80fbd415cad580e9b89c16b917aeeddbd2b58da
Woo.DongHoon <dhwoo@nhn.com>
2012-12-14 16:57:13 +0900

    no message

---
+++ src/com/happyty/fortunelottery/lotto/LottoNumberSao.java
@@ -0,0 +1,8 @@
+package com.happyty.fortunelottery.lotto;
+
+public class LottoNumberSao {
+	private final static String LOTTO645_SITE = "http://www.645lotto.net";
+	private final static String API_LATEST_URL = "/resultall/dummy.asp";
+	private final static String API_REQUEST_URL = "/ajax_jsonNew.asp?sGameNo=";
+
+}
