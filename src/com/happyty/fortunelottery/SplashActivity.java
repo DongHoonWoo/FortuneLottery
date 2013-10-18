9d05746402a97950a18547a038a486cb2afcae8c
Woo.DongHoon <dhwoo@nhn.com>
2013-10-18 22:51:24 +0900

    splash activity 추가

--- src/com/happyty/fortunelottery/SplashActivity.java
+++ src/com/happyty/fortunelottery/SplashActivity.java
@@ -1,7 +1,27 @@
 package com.happyty.fortunelottery;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends Activity {
+	private Handler handler = new Handler();
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+
+		handler.postDelayed(new Runnable() {
+
+			@Override
+			public void run() {
+				Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+				startActivity(intent);
+				finish();
+			}
+
+		}, 2000);
+	}
 }
